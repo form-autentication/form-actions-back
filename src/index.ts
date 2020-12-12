24662f45267371a19a919179b7fe0362e3ba48dc
Wdavid Calsin <david2c142@gmail.com>
2020-12-11 23:30:11 -0500

    refactor: 💡 added connection with mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
 import express from "express";
+import dotenv from "dotenv";
+import bodyParse from "body-parser";
+import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
+import { error } from "console";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get("/", function (req, res) {
-   res.send("Hello world");
+MongoClient.connect(
+   "mongodb+srv://form-actions:form-actions@cluster0.huo7v.mongodb.net/form-actions?retryWrites=true&w=majority",
+   {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+   }
+)
+   .then((client) => {
+      const db = client.db("form-actions");
+
+      const quotesCollection = db.collection("quotes");
+
+      app.post("/quotes", (req, res) => {
+         quotesCollection
+            .insertOne(req.body)
+            .then((result) => {
+               console.log(result);
+               res.redirect("/");
+            })
+            .catch((error) => console.error(error));
+      });
+
+      // console.log("Conected database: ", db);
+   })
+   .catch((error) => console.error(error));
+
+app.use(bodyParse.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+   res.send("Si funciono correctamente desde mi casa");
 });
 
-app.listen(3000);
+app.listen(port, () => console.log(`Esta conectado en el puerto ${port}`));
