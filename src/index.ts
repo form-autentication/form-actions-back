c5635c3cb446651f0aa4e4ac0c94f57b2c9910e3
WDavid Calsin <david2c142@gmail.com>
2020-12-13 23:34:30 -0500

    feat: 🎸 added the configuration (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,43 @@
 import express from "express";
-import dotenv from "dotenv";
 import bodyParse from "body-parser";
-import mongoose from "mongoose";
 import { MongoClient } from "mongodb";
-import { error } from "console";
+import config from "./config";
 
-dotenv.config();
+const startServer = async () => {
+   const app = express();
 
-const port = process.env.PORT || 8000;
-
-const app = express();
-
-MongoClient.connect(
-   "mongodb+srv://form-actions:form-actions@cluster0.huo7v.mongodb.net/form-actions?retryWrites=true&w=majority",
-   {
+   MongoClient.connect(config.databaseURL as any, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-   }
-)
-   .then((client) => {
-      const db = client.db("form-actions");
-
-      const quotesCollection = db.collection("quotes");
-
-      app.post("/quotes", (req, res) => {
-         quotesCollection
-            .insertOne(req.body)
-            .then((result) => {
-               console.log(result);
-               res.redirect("/");
-            })
-            .catch((error) => console.error(error));
-      });
-
-      // console.log("Conected database: ", db);
    })
-   .catch((error) => console.error(error));
+      .then((client) => {
+         const db = client.db("form-actions");
 
-app.use(bodyParse.urlencoded({ extended: true }));
+         const quotesCollection = db.collection("quotes");
 
-app.get("/", (req, res) => {
-   res.send("Si funciono correctamente desde mi casa");
-});
+         app.post("/quotes", (req, res) => {
+            quotesCollection
+               .insertOne(req.body)
+               .then((result) => {
+                  console.log(result);
+                  res.redirect("/");
+               })
+               .catch((error) => console.error(error));
+         });
 
-app.listen(port, () => console.log(`Esta conectado en el puerto ${port}`));
+         // console.log("Conected database: ", db);
+      })
+      .catch((error) => console.error(error));
+
+   app.use(bodyParse.urlencoded({ extended: true }));
+
+   app.get("/", (req, res) => {
+      res.send("Si funciono correctamente desde mi casa");
+   });
+
+   app.listen(config.port, () =>
+      console.log(`Esta conectado en el puerto ${config.port}`)
+   );
+};
+
+startServer();
