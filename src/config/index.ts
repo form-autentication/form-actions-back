8de399d6caf3a3fea15f62238699f5b72dbf8748
Wdavid Calsin <david2c142@gmail.com>
2020-12-13 23:33:25 -0500

    feat: 🎸 added the configuration

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+   throw new Error(" Couldn't find .env file Err");
+}
+
+export default {
+   port: process.env.PORT,
+
+   portFront: process.env.PORT_FRONT,
+
+   databaseURL: process.env.MONGODB_URI,
+};
