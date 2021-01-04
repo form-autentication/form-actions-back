6708d3ba662bc668a75a3ee52d0545a966e6660f
WDavid Calsin <david2c142@gmail.com>
2021-01-03 23:26:18 -0500

    refactor: 💡 refactor (#7)

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,7 +13,7 @@ export class Routes {
    constructor() {}
 
    public routes(app: any): void {
-      app.route("/").get(cors(), (req: Request, res: Response) => {
+      app.route("/profile").get(cors(), (req: Request, res: Response) => {
          getAllUsers(res);
       });
 
@@ -22,12 +22,11 @@ export class Routes {
             createUsers,
             (req: Request, res: Response, next: NextFunction) => {
                res.json({ message: "se añadieron correctatmente" });
-
                res.redirect(`${this.portFront}/`);
             }
          )
          .get((req: Request, res: Response) => {
-            res.send("Desde el el Quotes");
+            res.send("Desde e l el Quotes");
          });
 
       app.route("/login")
@@ -35,7 +34,6 @@ export class Routes {
             verifyUser,
             (req: Request, res: Response, next: NextFunction) => {
                console.log("Se llamo a la pagina login con el metodo POST");
-
                res.json({ msg: "Si se llamo al login" });
             }
          )
